0bdc9365409d87df572c70b901d05f8ca2494679
Henry O <107762735+devhenode@users.noreply.github.com>
2023-11-28 02:22:45 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./public/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'backgroundImage': './public/vectors/images/bg.png',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
